b995bfc4d8b8b05341137f22c641740a841689eb
Javier <javier.diosesz@gmail.com>
2024-11-06 18:17:39 -0500

    se implementa funcionlogin en javascript, valores por consola

---
+++ FuncionesLogin/funcionLogin.js
@@ -0,0 +1,8 @@
+function leerTexto(){
+    var usrN = document.getElementById("usrNm").value;
+    var passw = document.getElementById("Pswd").value;
+
+    //Mostrar valores
+    console.log(usrN);
+    console.log(passw);
+}
