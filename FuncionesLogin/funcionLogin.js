5f134e95be9fa7c71dd0d12747c00f5ea3bc363b
Javier <javier.diosesz@gmail.com>
2024-11-07 00:03:05 -0500

    Paginas agregadas - Secciones

--- FuncionesLogin/funcionLogin.js
+++ FuncionesLogin/funcionLogin.js
@@ -3,6 +3,23 @@ function leerTexto(){
     var passw = document.getElementById("Pswd").value;
 
     //Mostrar valores
-    console.log(usrN);
-    console.log(passw);
+    //console.log(usrN);
+    //console.log(passw);
+
+    var ingresar = auntenticar(usrN, passw)
+
+    if(ingresar){
+        alert("Bienvenido a la web de servicios de consultoría")
+        window.location.href="/Secciones/main.html"
+    } else {
+        alert("Password Incorrecto")
+    }
+}
+
+function auntenticar(username, passw){
+    if(username==="admin" && passw ==="passss34"){
+        return true;
+    } else {
+        return false;
+    }
 }
